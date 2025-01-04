36707e34d4953b50ba42db609682101e728c0c40
rubbenix <rubenix02@gmail.com>
2025-01-04 13:32:30 +0100

    testing Layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,13 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${montserrat.className} antialiased`}>
         {children}
+        <footer>
+          <p className="text-center text-white items-center justify-center bg-blue-500 p-4">
+            &copy; 2025 Laia ❤️. All rights reserved.
+          </p>
+        </footer>
       </body>
+
     </html>
   );
 }
