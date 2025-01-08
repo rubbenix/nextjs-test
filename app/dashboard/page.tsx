76535e78d96ca834cd775c271e897de0a9e8d5d9
rubbenix <rubenix02@gmail.com>
2025-01-08 17:36:11 +0100

    Suspensen,Streaming de datos

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,47 @@
-export default function Page(){
-    return <p>Contenido de la página Dashboard </p>
+import { Suspense } from "react";
+import { fetchCardData, fetchLatestInvoices, fetchRevenue } from "../lib/data";
+import { Card } from "../ui/dashboard/cards";
+import LatestInvoices from "../ui/dashboard/latest-invoices";
+import RevenueChart from "../ui/dashboard/revenue-chart";
+import { lusitana } from "../ui/fonts";
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from "../ui/skeletons";
+
+export default async function Page(){
+    //const revenue = await fetchRevenue(); //esta esperando a que el servidor le de la respuesta. si el servidor tarda 2 segundos, la pagina tardara 2 segundos en cargar
+    //console.log(revenue);
+    const latestInvoices = await fetchLatestInvoices();
+    const {
+        numberOfInvoices,
+        numberOfCustomers,
+        totalPaidInvoices,
+        totalPendingInvoices,
+      } = await fetchCardData();
+    return (
+        <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <Card title="Collected" value={totalPaidInvoices} type="collected" />
+        <Card title="Pending" value={totalPendingInvoices} type="pending" />
+        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+        <Card
+          title="Total Customers"
+          value={numberOfCustomers}
+          type="customers"
+        />
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart/>
+        </Suspense>
+
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices/>
+        </Suspense>
+
+        
+      </div>
+    </main>
+    );
 }
